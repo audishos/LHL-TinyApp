d46ac3003e81c4d48fffe7884cb656f01e5848ad
audishos <audisho.sada@gmail.com>
2017-09-13 14:36:43 -0400

    Changes object definitions in urlsDB.js to const

--- urlsDB.js
+++ urlsDB.js
@@ -1,9 +1,9 @@
 // our "database"
-let urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
   getAll: function() {
-    let all = {};
+    const all = {};
     for (key in this) {
       if (typeof this[key] !== 'function') {
         all[key] = this[key];
