a56c951b04d33d330fcdd4c9a0802b9eaa40e93a
audishos <audisho.sada@gmail.com>
2017-09-13 13:45:27 -0400

    Adds urlsDB.js - contains the 'database' and functions for CRUD

---
+++ urlsDB.js
@@ -0,0 +1,55 @@
+// our "database"
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+  getAll: function() {
+    let all = {};
+    for (key in this) {
+      if (typeof this[key] !== 'function') {
+        all[key] = this[key];
+      }
+    }
+    return all;
+  },
+  get: function(key) {
+    let data = {}
+    if (this[key]) {
+      data[key] = this[key];
+      return data;
+    } else {
+      return null;
+    }
+  },
+  add: function(key, value) {
+    if (!this[key] && value) {
+      this[key] = value;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  edit: function(key, value) {
+    if (this[key] && value) {
+      this[key] = value;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  delete: function(key) {
+    if (this[key]) {
+      delete this[key];
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+console.log(urlDatabase.getAll());
+console.log(urlDatabase.get("b2xVn2"));
+console.log(urlDatabase.add("b2xVn3", "https://www.reddit.com"));
+console.log(urlDatabase.edit("b2xVn2", "http://www.lhl.com"));
+console.log(urlDatabase.getAll());
+console.log(urlDatabase.delete("9sm5xK"));
+console.log(urlDatabase.getAll());
